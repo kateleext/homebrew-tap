459000998bd3eb2b4a4f911df270b79886d0b1ab
kate <83829378+kateleext@users.noreply.github.com>
2026-03-10 03:27:07 -0500

    Update perch to v0.0.5

--- Formula/perch.rb
+++ Formula/perch.rb
@@ -1,28 +1,28 @@
 class Perch < Formula
   desc "Minimal file viewer for coding agents"
   homepage "https://github.com/kateleext/perch"
-  version "0.0.4"
+  version "0.0.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_darwin_arm64.tar.gz"
-      sha256 "0281a807d8a8d4334a2bd9d69634a12863e19b46639ecfb39a70fbff328793ba"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.5/perch_darwin_arm64.tar.gz"
+      sha256 "efa02c8e8657fa3fe26ac30430adb390dc503b828f1b43d01991be39c3b36f63"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_darwin_amd64.tar.gz"
-      sha256 "8f196ea5795ca95f5f273e10110be8be222089ccfcaa59060794bee50b3f9d35"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.5/perch_darwin_amd64.tar.gz"
+      sha256 "f204524d90bd71d927dea3ef8bf475db169230f5d458592431bafb9bd323dc9f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_linux_arm64.tar.gz"
-      sha256 "21593a52d2bd4d8d692d7b8d06ef4403c18f515c22612e115ed9d9c5fb2c7b1b"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.5/perch_linux_arm64.tar.gz"
+      sha256 "404cd6fd949466faa7a54c48de45cf0e24628f939399d631b8ddee983e965b45"
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kateleext/perch/releases/download/v0.0.4/perch_linux_amd64.tar.gz"
-      sha256 "221a8e69ca4ecdbe5a90b344907b9a706092c587da863126d3cbe9d2dc710bdd"
+      url "https://github.com/kateleext/perch/releases/download/v0.0.5/perch_linux_amd64.tar.gz"
+      sha256 "0fab15f9f5b496e1e753a41c8f425582530426aadc48069f29da3f1c0bd55514"
     end
   end
 
